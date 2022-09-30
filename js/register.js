c513b8c5903a9381e11b6b0498ac25ccc8cbb184
Tey Chin Yi <72505694+snackasnack@users.noreply.github.com>
2022-09-30 16:01:38 +0800

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,13 +1,17 @@
-async function registerHandler(user_name, user_email, user_pwd) {
+async function registerHandler() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
+
+    console.log("trying to get element by id");
     
     var raw = JSON.stringify({
-      "email": user_email,
-      "name": user_name,
-      "password": user_pwd
+      "email": document.getElementById('user_email').value,
+      "name": document.getElementById('user_name').value,
+      "password": document.getElementById('user_pwd').value
     });
-    
+
+    console.log('get element by id successful');
+
     var requestOptions = {
       method: 'POST',
       headers: myHeaders,
